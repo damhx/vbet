b622334698aced80442c12dd5aa825ccfbeb03e1
David Medina <davidamedinahx@gmail.com>
2025-10-03 15:53:36 -0500

    feat: Adelanto html & css
    Adelanto html & css

--- src/app/core/RenderCore/home-component/home-component.ts
+++ src/app/core/RenderCore/home-component/home-component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ChipsComponent } from "./component/chips-component/chips-component";
+import { CardComponent } from "./component/card-component/card-component";
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
+  imports: [ChipsComponent, CardComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css'
 })
